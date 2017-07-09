b0a50485d224ebeaffe2efb8aa703e59d84ee9a2
bradonf333 <bradonf123@gmail.com>
2017-07-08 20:38:52 -0600

    Added product class and desc of class

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        
+    }
+}
