3bc71be452bb96b6a4f81df202d754f4e6c25d6b
bradonf333 <bradonf123@gmail.com>
2017-07-09 10:18:53 -0600

    Regular and Param constructor, aslo comments

--- Acme.Biz/Product.cs
+++ Acme.Biz/Product.cs
@@ -11,18 +11,38 @@ namespace Acme.Biz
     /// </summary>
     public class Product
     {
+        // Private Fields
         private string productName;
         private string description;
         private int productId;
 
+        // Properties
         public string ProductName { get; set; }
         public string Description { get; set; }
         public int ProductId { get; set; }
 
+        // Simple message to display product info
         public string SayHello()
         {
             return $"Hello {ProductName} ({ProductId}): {Description}";
         }
 
+        // Default Constructor - prints message
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        // Param Constructor to initialize properties and write a message
+        public Product(int productId, string productName, string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine($"Product instance has a name: {ProductName}");
+        }
+
     }
-}
+} 
+  
